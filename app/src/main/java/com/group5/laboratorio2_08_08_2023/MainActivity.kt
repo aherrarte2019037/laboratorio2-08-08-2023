367e5bea0b697c65a5af36be409ed32cce35ab09
Angel <angelherrarte3@gmail.com>
2023-08-02 19:01:10 -0600

    Se creo el evento al presionar un digito

--- app/src/main/java/com/group5/laboratorio2_08_08_2023/MainActivity.kt
+++ app/src/main/java/com/group5/laboratorio2_08_08_2023/MainActivity.kt
@@ -1,46 +1,35 @@
 package com.group5.laboratorio2_08_08_2023
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.group5.laboratorio2_08_08_2023.ui.theme.Laboratorio208082023Theme
+import android.view.View
+import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Laboratorio208082023Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+    }
+
+    fun pressDigit(view: View) {
+        val txtNum2: TextView = findViewById(R.id.textView2);
+        var num2 = txtNum2.text.toString();
+
+        when(view.id) {
+            R.id.btn0 -> txtNum2.text = num2.toString() + "0"
+            R.id.btn1 -> txtNum2.text = num2.toString() + "1"
+            R.id.btn2 -> txtNum2.text = num2.toString() + "2"
+            R.id.btn3 -> txtNum2.text = num2.toString() + "3"
+            R.id.btn4 -> txtNum2.text = num2.toString() + "4"
+            R.id.btn5 -> txtNum2.text = num2.toString() + "5"
+            R.id.btn6 -> txtNum2.text = num2.toString() + "6"
+            R.id.btn7 -> txtNum2.text = num2.toString() + "7"
+            R.id.btn8 -> txtNum2.text = num2.toString() + "8"
+            R.id.btn9 -> txtNum2.text = num2.toString() + "9"
+            R.id.btnPoint -> {
+                if (txtNum2.text.contains(".")) return;
+                txtNum2.text = "$num2."
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Laboratorio208082023Theme {
-        Greeting("Android")
     }
 }
